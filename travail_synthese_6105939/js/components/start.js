e6393561b7c494f755b998e3559037c6ae237a3b
raphy161192 <rlirette161192@outlook.com>
2020-03-15 23:10:54 -0400

    delete

--- travail_synthese_6105939/js/components/start.js
+++
@@ -1,6 +0,0 @@
-import avecTemplateHtml from '../avecTemplateHtml.js'
-
-Vue.component('start', avecTemplateHtml({
-    props: ['show'],
-    template: 'start.html',
-}))
--- travail_synthese_6105939/js/components/victory.js
+++
@@ -1,6 +0,0 @@
-import avecTemplateHtml from '../avecTemplateHtml.js'
-
-Vue.component('victory', avecTemplateHtml({
-    props: ['show', 'winner'],
-    template: 'victory.html'
-}))
